0faa2529238926a43373bfd8ca77012cb553e564
Yassinelaadraoui <yassinelaadraoui@gmail.com>
2017-03-27 11:24:55 +0300

    changes that add rating and picture to the mix

--- blank/www/js/controllers.js
+++ blank/www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers', [])
+angular.module('app.controllers', ['ionic.rating'])
 
 .controller('dashCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
@@ -12,11 +12,12 @@ $scope.openFilters= function(hasFilters){
   $scope.hasFilters = !$scope.hasFilters;
 }
 }])
-.controller('myMessagesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('myMessagesCtrl', ['$scope','$state', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
+function ($scope, $state, $stateParams) {
+if( 1)
+    $state.go('tabsController.dash');
 
 }])
 
@@ -24,7 +25,10 @@ function ($scope, $stateParams) {
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-
+  $scope.rating = {};
+  $scope.rating.rate = 5;
+  $scope.rating.max = 5;
+  $scope.readOnly = true;
 
 }])
 
@@ -33,6 +37,21 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 $scope.showingmessage=0;
+  $scope.hide = 0;
+
+$scope.hidecover = function(){
+  //function hide the cover that contains the list of elements a host would give to the guest ex wifi
+  if($scope.hide==0){
+      $scope.hide=1;
+
+  }else $scope.hide=0;
+
+}
+  $scope.rating = {};
+  $scope.rating.rate = 3;
+  $scope.rating.max = 5;
+
+
 
 }])
 
@@ -56,6 +75,7 @@ function ($scope, $stateParams) {
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
+
 
 
 }])
